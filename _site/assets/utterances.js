f3d7aede7a8cfbaaa54aca7820d79651132e6982
etiennebacher <etienne.bacher@protonmail.com>
2026-03-03 18:27:09 +0100

    push the rest of _site

---
+++ _site/assets/utterances.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  if (!/\/posts\//.test(location.pathname)) {
+    return;
+  }
+
+  var script = document.createElement("script");
+  script.src = "https://utteranc.es/client.js";
+  script.setAttribute("repo", "etiennebacher/personal_website_distill");
+  script.setAttribute("issue-term", "og:title");
+  script.setAttribute("crossorigin", "anonymous");
+  script.setAttribute("label", "comment_thread");
+
+  var article = document.querySelector("article");
+  if (article) {
+    article.appendChild(script);
+  }
+});
